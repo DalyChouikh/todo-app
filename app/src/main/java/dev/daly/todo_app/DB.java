f8905bf35c3c4b131942763c3b6b1b680d1d1862
DalyChouikh <chouikhdaly215@gmail.com>
2023-12-01 19:56:41 +0100

    set viewBinding to true in build.gradle

--- app/src/main/java/dev/daly/todo_app/DB.java
+++ app/src/main/java/dev/daly/todo_app/DB.java
@@ -1,2 +1,47 @@
-package dev.daly.todo_app;public class DB {
+package dev.daly.todo_app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DB extends SQLiteOpenHelper {
+
+    public DB(@Nullable Context context) {
+        super(context, "users.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase myDB) {
+        myDB.execSQL("CREATE TABLE users (username TEXT, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase myDB, int i, int i1) {
+        myDB.execSQL("DROP TABLE IF EXISTS users");
+    }
+
+    public void insertUser(String username, String password) {
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = myDB.insert("users", null, contentValues);
+    }
+
+    public void deleteUser(String username) {
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        long result = myDB.delete("users", "username=?", new String[]{username});
+    }
+
+    public void updateUser(String username, String password) {
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("password", password);
+        long result = myDB.update("users", contentValues, "username=?", new String[]{username});
+    }
+
+
 }
